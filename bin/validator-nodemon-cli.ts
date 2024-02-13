a09cf18e5d71633044a5990b963852cf2a154556
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-13 21:04:24 +0200

    Added nodemon version.

---
+++ bin/validator-nodemon-cli.ts
@@ -0,0 +1,39 @@
+#!/usr/bin/env nodemon
+
+import message from "@euriklis/message-ts"
+import fs from "fs";
+import path from "path";
+
+const args = process.argv.slice(2);
+const dirname = args[0] || "Tests";
+
+const testsFolder = path.join(process.cwd(), dirname);
+// Check if the "Tests" folder exists, otherwise try "tests"
+if (!fs.existsSync(testsFolder)) {
+  new message()
+    .bold
+    .underline
+    .setBgColor("red")
+    .setColor("black")
+    .append(`No tests files in the folder ${testsFolder}.`)
+    .reset
+    .log();
+  process.exit(1);
+} else console.log(testsFolder);
+
+// Find all test files ending with "spec.js" and run them using Node.js
+fs.readdirSync(testsFolder).forEach((file) => {
+  if (file.endsWith("spec.ts")) {
+    import(path.join(testsFolder, file))
+      .catch((err) => {
+        new message()
+          .bold
+          .italic
+          .setColor("red")
+          .appendWarningSign
+          .append(`Error in testing of the ${testsFolder}/${file}`)
+          .append("\n")
+          .reset.log();
+      });
+  }
+});
